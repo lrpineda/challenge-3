b61cc25fe735d397d10af521779e7c78126db935
lrpineda <luicks212@gmail.com>
2022-04-03 18:06:24 -0600

    fixing issue with adding lower case and uppercase

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ var special = [
   "~",
   '"',
 ];
-// Numbers 
+// Numbers
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Generate password fuction
@@ -89,29 +89,45 @@ var generatePassword = function () {
     return theLength;
   };
 
-  // Confirm if user wants uppercase on some of the characters
-  var getCase = function () {
-    return confirm(
-      "Would you like to include uppercase characters?\n(By default it is all lowercase.)"
-    );
+  // Confirm if user wants uppercase
+  var getUpperCase = function () {
+    var wantsUpperCase = confirm;
+    return confirm("Would you like to include uppercase characters?");
+  };
+  // Confirm if user wants lowercase
+  var getLowerCase = function () {
+    return confirm("Would you like to include lowercase characters?");
   };
   // Confirm if the user wants numers
   var getNumeric = function () {
-    return confirm(
-      "Would you like to add numbers?\n(By default it only letters.)"
-    );
+    return confirm("Would you like to add numbers as part of the password?");
   };
   // Confirm if the user wants special characters
   var getSpecial = function () {
-    return confirm("Would you like special characters?");
+    return confirm(
+      "Would you like special characters as part of the password?"
+    );
   };
 
   // Setting the criteria object
   var criteria = {
     Length: getLength(),
-    uppercase: getCase(),
+    uppercase: getUpperCase(),
+    lowercase: getLowerCase(),
     numeric: getNumeric(),
     specialCharacters: getSpecial(),
+
+    //Function to check if at least lowercase or uppercase option is true
+    checkCapitalization: function () {
+      if (!this.uppercase && !this.lowercase) {
+        window.alert(
+          "You must select at least one either lowercase or uppercase characters"
+        );
+        this.uppercase = getUpperCase();
+        this.lowercase = getLowerCase();
+        this.checkCapitalization();
+      }
+    },
 
     // function to randomize a character depending on the criteria set by the user
     randomizer: function () {
@@ -122,11 +138,18 @@ var generatePassword = function () {
       character = "";
 
       // Confirm if the user wants uppercase letters
-      if (this.uppercase) {
+      if (this.uppercase && this.lowercase) {
         // Randomize the times letters are uppercase
         if (Math.random() > 0.5) {
           holder += randomAlpha.toUpperCase();
+          console.log("Inside if uppercase and lowercase");
         }
+      } else if (this.uppercase && !this.lowercase) {
+        holder += randomAlpha.toUpperCase();
+        console.log("Inside only uppercase");
+      } else if (!this.uppercase && this.lowercase) {
+        holder += randomAlpha;
+        console.log("Inside only lowercase");
       }
 
       // Confirm if the user wants numbers
@@ -137,9 +160,8 @@ var generatePassword = function () {
       if (this.specialCharacters) {
         holder += randomSpecial;
       }
-      holder += randomAlpha;
 
-      // Once we have one of each (one random number, one random letter, one special character) 
+      // Once we have one of each (one random number, one random letter, one special character)
       // we will randomize it again to only return one character
       if (holder.length > 1) {
         character = holder[Math.floor(Math.random() * holder.length)];
@@ -151,6 +173,8 @@ var generatePassword = function () {
   };
 
   var generatedPass = "";
+  //Check capitalization before generating password
+  criteria.checkCapitalization();
   //Based on the criteria's length, we will generate a password with one random character at a time.
   for (var i = 0; i < criteria.Length; i++) {
     generatedPass += criteria.randomizer();
